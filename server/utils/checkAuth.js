b03d0ec015b82fd173b39b556f5fcc9b49fbaa93
Denys Izhetskyi <denys.izhetskyi@booking.com>
2019-01-12 23:33:30 +0100

    remove 'bearer' from token

--- server/utils/checkAuth.js
+++ server/utils/checkAuth.js
@@ -4,10 +4,15 @@ const db = require('../db');
 
 const checkAuth = async (req, res, next) => {
   try {
-    const token =
+    let token =
       req.body.authToken || req.query.authToken || req.headers['authorization'];
+
     if (!token) {
       throw new Error('No auth token');
+    }
+
+    if (token.startsWith('Bearer ')) {
+      token = token.slice(7);
     }
 
     const decoded = await jwt.verify(token, req.app.get('secret'));
