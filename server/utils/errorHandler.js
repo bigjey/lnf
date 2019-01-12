6de35daa1b090b5940afe055d2fab7a507af3280
Denys Izhetskyi <denys.izhetskyi@booking.com>
2019-01-12 23:33:44 +0100

    better sequelize error handling

--- server/utils/errorHandler.js
+++ server/utils/errorHandler.js
@@ -1,4 +1,17 @@
+const sequelize = require('sequelize');
+
 function errorHandler(error, req, res, next) {
+  if (error instanceof sequelize.ValidationError) {
+    res.status(400);
+
+    return res.json(
+      error.errors.map((err) => ({
+        message: err.message,
+        path: err.path
+      }))
+    );
+  }
+
   if (res.statusCode === 200) {
     res.status(500);
   }
