93c5d4e707aae6c881d2533b9d2ce22d07e9d08d
Denys Izhetskyi <denys.izhetskyi@booking.com>
2019-01-12 16:06:25 +0100

    auth controller styling

--- server/api/auth/controller.js
+++ server/api/auth/controller.js
@@ -1,32 +1,27 @@
-const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const db = require('../../db');
 
 const jwtOptions = {
-  expiresIn: '5m'
+  expiresIn: '7d'
 };
 
 const login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
-
     const where = { email };
 
     const user = await db.User.findOne({ where });
-
     if (!user) {
       throw new Error('Wrong email or password');
     }
 
     const isMatch = await user.comparePassword(password);
-
     if (!isMatch) {
       throw new Error('Wrong email or password');
     }
 
     const payload = { userId: user.id };
-
     const token = jwt.sign(payload, req.app.get('secret'), jwtOptions);
 
     res.json({ token });
@@ -50,11 +45,8 @@ const register = async (req, res, next) => {
 const validate = async (req, res, next) => {
   try {
     const { token } = req.body;
-
     const decoded = await jwt.verify(token, req.app.get('secret'));
-
     const payload = { userId: decoded.userId };
-
     const refreshedToken = jwt.sign(payload, req.app.get('secret'), jwtOptions);
 
     res.json({ token: refreshedToken });
