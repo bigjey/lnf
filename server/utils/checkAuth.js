ad0aa0b49e657c426cb30387e38d1d88a87de9d8
Denys Izhetskyi <denys.izhetskyi@booking.com>
2019-01-12 16:04:43 +0100

    add checkAuth middleware

---
+++ server/utils/checkAuth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+
+const db = require('../db');
+
+const checkAuth = async (req, res, next) => {
+  try {
+    const token =
+      req.body.authToken || req.query.authToken || req.headers['authorization'];
+    if (!token) {
+      throw new Error('No auth token');
+    }
+
+    const decoded = await jwt.verify(token, req.app.get('secret'));
+
+    const user = await db.User.findByPk(decoded.userId);
+    if (!user) {
+      throw new Error('No user data');
+    }
+
+    req.user = user;
+
+    next();
+  } catch (err) {
+    res.status(401);
+
+    return next(err);
+  }
+};
+
+module.exports = checkAuth;
