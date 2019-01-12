9d7dd60fe305286ae5cb192b6715ed2cf43e47aa
serhiikatrevych <serhii@harver.com>
2019-01-12 23:43:41 +0100

    docs for GET /api/post

---
+++ app/src/screens/InitScreen/InitScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class InitScreen extends Component {
+
+    componentDidMount() {
+
+    }
+
+    render () {
+        return null;
+    }
+}
+
+export default InitScreen;
