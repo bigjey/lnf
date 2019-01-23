a2bbdd53fc4d14fdd3c4ea3505bca6cf605a58d2
Denys Izhetskyi <denys.izhetskyi@booking.com>
2019-01-24 00:40:31 +0100

    show image on post details

--- app/src/screens/PostDetails/PostDetails.js
+++ app/src/screens/PostDetails/PostDetails.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import { inject, observer } from 'mobx-react';
 
 const PostDetails = ({
   store: {
-    activePost: { id, description, gender, breed },
+    activePost: { id, description, gender, breed, image },
   },
 }) => (
-  <View style={styles.container}>
-    <Text>Post Details</Text>
-    <Text>id: {id}</Text>
-    {description && <Text>{description}</Text>}
-    {breed && <Text>{breed}</Text>}
-    {gender && <Text>{gender}</Text>}
-  </View>
-);
+    <View style={styles.container}>
+      <Text>Post Details</Text>
+      <Text>id: {id}</Text>
+      {description && <Text>{description}</Text>}
+      {breed && <Text>{breed}</Text>}
+      {gender && <Text>{gender}</Text>}
+      {image && <Image
+        style={{ width: 128, height: 128 }}
+        source={{ uri: image }}
+      />}
+    </View>
+  );
 
 const styles = StyleSheet.create({
   container: {
