7e50edd572a69f8e2c9f9398f7c91ea2f5af9403
Denys Izhetskyi <denys.izhetskyi@booking.com>
2019-05-19 15:12:03 +0200

    posts pagination, order, more filters

--- server/api/post/controller.js
+++ server/api/post/controller.js
@@ -28,17 +28,53 @@ const createPost = async (req, res, next) => {
   }
 };
 
+const ORDER_FIELDS = ['createdAt'];
+const ORDER_DIRECTIONS = ['desc', 'asc'];
+const POSTS_PER_PAGE = 2;
+
 const getAllPosts = async (req, res, next) => {
   try {
-    const { userId } = req.query;
+    const { userId, order: orderValue, page = 1, breed, gender } = req.query;
+
+    let p = parseInt(page, 10);
+    if (Number.isNaN(p) || p < 1) {
+      throw new Error('page param must be a positive integer');
+    }
 
     const criteria = {};
+
+    let order = ['createdAt', 'desc'];
+    const limit = POSTS_PER_PAGE;
+    const offset = (page - 1) * POSTS_PER_PAGE;
 
     if (userId) {
       criteria.userId = userId;
     }
 
-    const posts = await db.Post.findAll({ where: criteria });
+    if (breed) {
+      criteria.breed = breed;
+    }
+
+    if (gender) {
+      criteria.gender = gender;
+    }
+
+    if (orderValue) {
+      let [f, d] = orderValue.split('_');
+      if (ORDER_FIELDS.includes(f) && ORDER_DIRECTIONS.includes(d)) {
+        order = [f, d];
+      }
+    }
+
+    const { count, rows: posts } = await db.Post.findAndCountAll({
+      where: criteria,
+      order: [order],
+      limit,
+      offset,
+    });
+
+    res.set('x-total', count);
+    res.set('x-page-size', POSTS_PER_PAGE);
 
     posts.forEach(p => {
       if (p.image) {
